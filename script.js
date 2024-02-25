84f12eae4df1f4382737195b510dab928132d9ab
PANKAJ SHARMA <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2024-02-25 13:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+ JavaScript code (fibonacci.js)
 function fibonacci(num) {
-// your code here
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    let a = 0, b = 1, c;
+
+    for (let i = 2; i <= num; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
 }
 
-module.exports = fibonacci;
+function calculateFibonacci() {
+    const numInput = document.getElementById('numInput');
+    const resultElement = document.getElementById('result');
+
+    const num = parseInt(numInput.value);
+
+    if (isNaN(num) || num < 0 || num > 50) {
+        resultElement.textContent = 'Please enter a valid number between 0 and 50.';
+    } else {
+        const result = fibonacci(num);
+        resultElement.textContent = `The ${num}th Fibonacci number is: ${result}`;
+    }
+}
